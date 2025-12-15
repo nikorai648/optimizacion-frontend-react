a3f995d77c8ffe6a8fcd0b5d5eb89e49f056e4c9
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-15 19:57:50 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/css/estilos.css"; // tu css
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
